f3c2de1b88bba2261e74fecb1219a4cdb2dd8969
Zeikar <antstp@naver.com>
2017-12-17 01:33:03 +0900

    final Commit
    20171217

--- GoChat/src/chat/chat.go
+++ GoChat/src/chat/chat.go
@@ -8,19 +8,22 @@ import(
 	"net/http"
 	"time"
 	"fmt"
+	"strconv"
 
 	"github.com/googollee/go-socket.io" // socket.io 패키지 사용
 )
 
 // 전역 변수
 var (
-	subscribe = make(chan (chan<- Subscription), 10)// 구독 채널
-	unsubscribe = make(chan (<-chan Event), 10)		// 구독 해지 채널
-	publish = make(chan Event, 10)					// 이벤트 발행 채널
+	subscribe = map[int]chan (chan<- Subscription){}// 구독 채널
+	unsubscribe = map[int]chan (<-chan Event){}		// 구독 해지 채널
+	publish = map[int]chan Event{}					// 이벤트 발행 채널
 
 	register = make(chan User, 10)				// 사용자 가입 채널
 	login = make(chan User, 10)					// 사용자 로그인 채널
 	loginCheck = make(chan LoginCheckRequest, 10)// 로그인 되어있는지 확인 채널
+
+	chatroomCount = 0
 )
 
 // 로그인한 유저 정보 요청
@@ -42,6 +45,7 @@ type Event struct {
 	UserId string	// 사용자 이름
 	Timestamp int	// 시간 값
 	Text string		// 메시지 텍스트
+	ChatroomNo int	// 채팅방 번호
 }
 
 // 구독 구조체 정의
@@ -50,6 +54,7 @@ type Subscription struct {
 	Archive []Event		// 지금까지 쌓인 이벤트를 저장할 슬라이스
 	New chan Event		// 새 이벤트가 생길 때마다 데이터를 받을 수 있도록
 						// 이벤트 채널 생성
+	ChatroomNo int		// 현재 채팅방 번호
 }
 
 // 사용자 구조체 생성 함수
@@ -59,20 +64,20 @@ func NewUser(userId, userName string, userChan chan Event) User {
 
 // 이벤트 생성 함수
 // Event 구조체 인스턴스 생성. 현재 시각으로 세팅
-func NewEvent(evtType, userId, msg string) Event {
-	return Event{evtType, userId, int(time.Now().Unix()), msg}
+func NewEvent(evtType, userId, msg string, chatroomNo int) Event {
+	return Event{evtType, userId, int(time.Now().Unix()), msg, chatroomNo}
 }
 
 // 새로운 사용자가 들어왔을 때 이벤트를 구독할 함수
-func Subscribe() Subscription {
+func Subscribe(chatroomNo int) Subscription {
 	c := make(chan Subscription)	// 채널 생성
-	subscribe <- c					// 구독 채널에 보냄
+	subscribe[chatroomNo] <- c		// 구독 채널에 보냄
 	return <-c
 }
 
 // 사용자가 나갔을 때 구독을 취소할 함수
-func (s Subscription) Cancel() {
-	unsubscribe <- s.New	// 구독 해지 채널에 보냄
+func (s Subscription) Cancel(chatroomNo int) {
+	unsubscribe[chatroomNo] <- s.New	// 구독 해지 채널에 보냄
 
 	for {	// 무한 루프
 		select {
@@ -128,27 +133,27 @@ func GetUserName(userId string) string{
 }
 
 // 사용자가 들어왔을 때 이벤트 발행
-func Join(userName string) {
+func Join(userName string, chatroomNo int) {
 	// 사용자 이름으로 join 이벤트를 만들고 publish 채널로 보냄
-	publish <- NewEvent("join", userName, "")
+	publish[chatroomNo] <- NewEvent("join", userName, "", chatroomNo)
 
-	fmt.Println(userName + " user joined")
+	fmt.Println(strconv.Itoa(chatroomNo) + " : " + userName + " user joined")
 }
 
 // 사용자가 채팅 메시지를 보냈을 때 이벤트 발행
-func Say(userName, message string) {
+func Say(userName, message string, chatroomNo int) {
 	// 사용자 이름, 메시지로 message 이벤트를 만들고 publish 채널로 보냄
-	publish <- NewEvent("message", userName, message)
+	publish[chatroomNo] <- NewEvent("message", userName, message, chatroomNo)
 
-	fmt.Println(userName + " user said " + message)
+	fmt.Println(strconv.Itoa(chatroomNo) + " : " + userName + " user said " + message)
 }
 
 // 사용자가 나갔을 때 이벤트 발행
-func Leave(userName string) {
+func Leave(userName string, chatroomNo int) {
 	// 사용자 이름으로 leave 이벤트를 만들고 publish 채널로 보냄
-	publish <- NewEvent("leave", userName, "")
+	publish[chatroomNo] <- NewEvent("leave", userName, "", chatroomNo)
 
-	fmt.Println(userName + " user left")
+	fmt.Println(strconv.Itoa(chatroomNo) + " : " + userName + " user left")
 }
 
 // 사용자 회원가입 및 로그인 처리할 함수
@@ -183,14 +188,11 @@ func UserManager() {
 				fmt.Println(newUser.UserId + " " + newUser.UserName + " user logined")
 	
 				// 유저에게 로그인 성공 메시지를 보냄
-				newUser.UserChan <- NewEvent("loginsuccess", newUser.UserName, "")
-				
-				Join(newUser.UserName)	// 사용자가 채팅방에 들어왔다는 이벤트 발행
-										// so.Id()는 socket.io의 세션 ID	
+				newUser.UserChan <- NewEvent("loginsuccess", newUser.UserName, "", 0)
 			} else {
 				fmt.Println(newUser.UserId + " " + newUser.UserName + " user failed")
 				// 유저에게 로그인 실패 메시지를 보냄
-				newUser.UserChan <- NewEvent("loginfailed", newUser.UserName, "")
+				newUser.UserChan <- NewEvent("loginfailed", newUser.UserName, "", 0)
 			}
 
 		// 유저가 로그인 되어있는지 확인 요청하면 loginCheck으로 값 전송됨
@@ -205,13 +207,29 @@ func UserManager() {
 }
 
 // 구독, 구독 해지, 발행된 이벤트를 처리할 함수
-func Chatroom() {
+// 채팅방 처리
+func Chatroom(chatroomNo int) {
+
+	if _, ok := subscribe[chatroomNo]; !ok {
+		// 채널들 초기화
+		subscribe[chatroomNo] = make(chan (chan<- Subscription), 10)
+		unsubscribe[chatroomNo] = make(chan (<-chan Event), 10)
+		publish[chatroomNo] = make(chan Event, 10)
+
+		fmt.Println(strconv.Itoa(chatroomNo) + " chat room created")
+
+		chatroomCount++
+	} else {
+		// 이미 생성되어있는 채팅방이면 고루틴 종료
+		return
+	}
+
 	archive := list.New()		// 쌓인 이벤트를 저장할 연결 리스트
 	subscribers := list.New()	// 구독자 목록을 저장할 연결 리스트
 
 	for {	// 무한 루프
 		select {
-		case c := <-subscribe:	// 새로운 사용자가 들어왔을 때 -> subscribe에 값이 들어옴
+		case c := <-subscribe[chatroomNo]:	// 새로운 사용자가 들어왔을 때 -> subscribe에 값이 들어옴
 			var events []Event	// 이벤트 슬라이스
 
 			for e := archive.Front(); e != nil; e = e.Next() {	// 쌓인 이벤트가 있다면
@@ -222,10 +240,10 @@ func Chatroom() {
 			subscriber := make(chan Event, 10)	// 이벤트 채널 생성
 			subscribers.PushBack(subscriber)	// 이벤트 채널을 구독자 목록에 추가
 
-			c <- Subscription{events, subscriber}	// 구독 구조체 인스턴스 생성하고
-													// 채널 c로 보냄
+			// 구독 구조체 인스턴스 생성하고 채널 c로 보냄
+			c <- Subscription{events, subscriber, chatroomNo}
 
-		case event := <-publish:	// 새 이벤트가 발행되었을 때 -> publish에 값이 들어옴 
+		case event := <-publish[chatroomNo]:	// 새 이벤트가 발행되었을 때 -> publish에 값이 들어옴 
 									// Join, Say, Leave
 			// 모든 사용자에게 이벤트 전달
 			for e := subscribers.Front(); e != nil; e = e.Next() {
@@ -243,7 +261,7 @@ func Chatroom() {
 			}
 			archive.PushBack(event)	// 현재 이벤트를 저장
 
-		case c := <-unsubscribe:	// 사용자가 나갔을 때 -> unsubscribe에 값이 들어옴 
+		case c := <-unsubscribe[chatroomNo]:	// 사용자가 나갔을 때 -> unsubscribe에 값이 들어옴 
 			for e := subscribers.Front(); e != nil; e = e.Next() {
 				// 구독자 목록에서 이벤트 채널을 꺼냄
 				subscriber := e.Value.(chan Event)
@@ -270,34 +288,61 @@ func main() {
 	// 사용자 관리 함수를 고루틴으로 실행
 	go UserManager()
 
-	go Chatroom() // 채팅방 처리 함수를 고루틴으로 실행
+	go Chatroom(0) // 채팅방 처리 함수를 고루틴으로 실행
+	chatroomCount--// 이 방은 채팅방이 아니고 통신 처리용
 
 	// 웹 브라우저에서 socket.io로 접속했을 때 실행할 콜백 설정
 	// On 함수로 각 상황마다 콜백 함수 실행
 	server.On("connection", func(so socketio.Socket) {
 		// 웹 브라우저가 접속되면
-		s := Subscribe()	// 구독 처리
+		s := Subscribe(0)	// 구독 처리
+		s.ChatroomNo = 0
 
 		fmt.Println(so.Id() + " user connected")
-
-		for _, event := range s.Archive {	// 지금까지 쌓인 이벤트를
-			so.Emit("event", event)			// 웹 브라우저로 접속한 사용자에게 보냄
-											// event 메시지로 보냄
-		}
 
 		// string 채널 생성
 		newMessages := make(chan string)
 
 		// 웹 브라우저에서 보내오는 회원 가입 요청을 받을 수 있도록 콜백
 		so.On("register", func(userName string) {
-			// 받은 메시지를 newMessages 채널로 보냄
+			// 사용자 회원가입
 			RegisterUser(so.Id(), userName, s.New)
 		})
 
 		// 웹 브라우저에서 보내오는 로그인 요청 받을 수 있도록 콜백
 		so.On("loginrequest", func(userName string) {
-			// 받은 메시지를 newMessages 채널로 보냄
+			// 사용자 로그인
 			LoginUser(so.Id(), userName, s.New)
+		})
+
+		// 웹 브라우저에서 보내오는 채팅 방 들어가는 요청
+		so.On("chatroom", func(chatroomStr string) {
+			
+			Leave(GetUserName(so.Id()), s.ChatroomNo)
+			// 구독 취소
+			s.Cancel(s.ChatroomNo)
+
+			chatroomNo, _ := strconv.Atoi(chatroomStr)
+
+			// 새 채팅방 생성
+			go Chatroom(chatroomNo)
+			time.Sleep(100 * time.Millisecond)
+			// 새 채팅방 구독
+			s = Subscribe(chatroomNo)	// 구독 처리
+			s.ChatroomNo = chatroomNo
+
+			for _, event := range s.Archive {	// 지금까지 쌓인 이벤트를
+				so.Emit("event", event)			// 웹 브라우저로 접속한 사용자에게 보냄
+												// event 메시지로 보냄
+			}
+
+			// 사용자가 채팅방에 들어왔다는 이벤트 발행
+			Join(GetUserName(so.Id()), chatroomNo)
+
+			// 현재 채팅방 개수 알려줌
+			for _, v := range publish {
+				v <- NewEvent("chatroomno", "", "", chatroomCount)
+			}		
 		})
 
 		// 웹 브라우저에서 보내오는 채팅 메시지를 받을 수 있도록 콜백
@@ -310,10 +355,10 @@ func main() {
 		so.On("disconnection", func() {
 			// 로그인한 유저는 사용자가 채팅방에서 나갔다는 이벤트 발행
 			if IsUserLogined(so.Id()) {
-				Leave(GetUserName(so.Id()))
+				Leave(GetUserName(so.Id()), s.ChatroomNo)
 			}
 			// 구독 취소
-			s.Cancel()
+			s.Cancel(s.ChatroomNo)
 
 			fmt.Println(so.Id() + " user disconnected")
 		})
@@ -326,8 +371,10 @@ func main() {
 				case event := <-s.New:		// 채널에 이벤트가 들어오면
 					so.Emit("event", event)	// 이벤트 데이터를 웹 브라우저에 보냄
 
+
 				case msg := <-newMessages:	// 웹 브라우저에서 채팅 메시지를 보내오면
-					Say(GetUserName(so.Id()), msg)		// 채팅 메시지 이벤트 발행
+					// 채팅 메시지 이벤트 발행
+					Say(GetUserName(so.Id()), msg, s.ChatroomNo)
 											// 사용자 아이디와 메시지 내용을 이벤트로 발행
 				}
 			}
